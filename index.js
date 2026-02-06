d29940e3354a14975b76341a8958828b9ff8cbb1
RuslanKuznetsovIT <ruslan.kser92@gmail.com>
2026-02-06 21:26:39 +0500

    Init

---
+++ index.js
@@ -0,0 +1,6 @@
+const button = document.querySelector("button");
+const output = document.querySelector(".output");
+
+button.addEventListener("click", function () {
+  output.innerHTML = "Жена, я тебя люблю!💕";
+});
